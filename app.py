60f9ab2d0ba83a925f73ef7b6275e35a9cf859db
AlShahriah <alshahriah2007@gmail.com>
2022-12-23 15:34:32 +0530

    Final Fixes

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 app = FastAPI()
 
-async def my_function():
+async def start_bot():
     await bot.start(os.getenv("TOKEN"))
 
 @app.on_event("startup")
 async def startup():
     loop = asyncio.get_event_loop()
-    # run the function only once
-    loop.run_until_complete(my_function())
+    loop.create_task(start_bot())
 
 @app.get("/")
 def home():
--- bot.py
+++ bot.py
@@ -3,14 +3,11 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!', description='A bot that does stuff.', intents=discord.Intents.all())
-extensions = []
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
     for filename in os.listdir('./cogs'):
-        if filename.endswith('.py') and not filename.startswith('_'):
+        if filename.endswith('.py'):
             await bot.load_extension(f'cogs.{filename[:-3]}')
-            extensions.append(filename[:-3])
     await bot.load_extension('jishaku')
-    print("Loaded extensions: " + ", ".join(extensions))
