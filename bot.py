85a7ea060bdb3855a6eb0fb1eb2714b97ea5479f
Alshahriah <alshahriah2007@gmail.com>
2023-02-25 18:20:43 +0530

    ✨

--- bot.py
+++ bot.py
@@ -3,21 +3,29 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from webserver import keep_alive
+import threading
+
 load_dotenv()
 
 bot = commands.Bot(command_prefix='!', description='A bot that does stuff.', intents=discord.Intents.all())
 token = os.getenv('DISCORD_TOKEN')
-has_run = bool
+
+# Create a lock object
+lock = threading.Lock()
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'cogs.{filename[:-3]}')
-    await bot.load_extension('jishaku')
+    with lock:
+        if not getattr(on_ready, "has_run", False):
+            on_ready.has_run = True
+            print(f'{bot.user.name} has connected to Discord!')
+            for filename in os.listdir('./cogs'):
+                if filename.endswith('.py'):
+                    await bot.load_extension(f'cogs.{filename[:-3]}')
+            await bot.load_extension('jishaku')
 
-if has_run == False:
-    keep_alive()
-    bot.run(token)
-has_run = True
+# Start the web server to keep the bot alive
+keep_alive()
+
+# Start the bot
+bot.run(token)
