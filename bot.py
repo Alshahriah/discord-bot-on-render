ab2998682c9153a9158818a9ebb8a7fb33d8ac5e
Alshahriah <alshahriah2007@gmail.com>
2023-02-25 17:41:02 +0530

    ✨

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+from webserver import keep_alive
 
 bot = commands.Bot(command_prefix='!', description='A bot that does stuff.', intents=discord.Intents.all())
+token = os.getenv('DISCORD_TOKEN')
 
 @bot.event
 async def on_ready():
@@ -11,3 +14,6 @@ async def on_ready():
         if filename.endswith('.py'):
             await bot.load_extension(f'cogs.{filename[:-3]}')
     await bot.load_extension('jishaku')
+
+keep_alive()
+bot.run(token)
