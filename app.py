a9dc31ad91c1926bf677b58a21fb67173582901b
Alshahriah <alshahriah2007@gmail.com>
2023-03-04 06:45:30 +0530

    ✨

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
-import os
-from fastapi import FastAPI
-import asyncio
-from bot import bot
-from dotenv import load_dotenv
+from flask import Flask
+app = Flask(__name__)
 
-load_dotenv()
-app = FastAPI()
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
-async def start_bot():
-    await bot.start(os.getenv("TOKEN"))
-
-@app.on_event("startup")
-async def startup():
-    loop = asyncio.get_event_loop()
-    loop.create_task(start_bot())
-
-@app.get("/")
-def home():
-    return "Hello World!"
+if __name__ == "__main__":
+    app.run()
